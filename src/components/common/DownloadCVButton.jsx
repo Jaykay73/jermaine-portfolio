22ad15c4b78df852bd18f90106336ca4337dac0e
Jaykay73 <jermainealedare@gmail.com>
2026-02-07 20:13:48 +0100

    Update CV file

--- src/components/common/DownloadCVButton.jsx
+++ src/components/common/DownloadCVButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FiDownload } from "react-icons/fi";
-import resume from "../../assets/resume/AledareJohnResume.pdf";
+import resume from "../../assets/resume/Aledare-John-Resume.pdf";
 
 const DownloadCVButton = ({ children }) => {
   return (
     <div className="flex flex-col xl:flex-row items-center gap-8 mt-4">
       <a
         href={resume}
-        download="AledareJohnResume.pdf"
+        download="Aledare-John-Resume.pdf"
         className="flex items-center gap-2 bg-black text-accent/90 px-6 py-3 rounded-lg cursor-pointer
               shadow-[var(--shadow-neon)] hover:scale-105 transition-all duration-300"
       >
